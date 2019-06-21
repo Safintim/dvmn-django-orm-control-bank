3fbf41a199b135341732c41e0403a67308f90bab
Safin Timur <timurtlt96@mail.ru>
2019-06-22 00:07:39 +0300

    Changed

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,14 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
