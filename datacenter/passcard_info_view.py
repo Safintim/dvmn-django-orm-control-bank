794e4bc8d9827a684d4422905917ba01619f995e
Safin Timur <timurtlt96@mail.ru>
2019-06-25 18:49:00 +0300

    Added duration for not leaved_at visit

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,6 +1,7 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
 import datetime
 
 
@@ -10,12 +11,14 @@ def passcard_info_view(request, passcode):
     this_passcard_visits = []
 
     for visit in visits:
+        data_visit = {
+            'entered_at': visit.entered_at,
+            'is_strange': False,
+            'duration': timezone.now() - visit.entered_at
+        }
         if visit.leaved_at:
-            data_visit = {
-                'entered_at': visit.entered_at,
-                'duration': visit.leaved_at - visit.entered_at,
-                'is_strange': False
-            }
+            data_visit['duration'] = visit.leaved_at - visit.entered_at
+
         if is_visit_long(visit):
             data_visit['is_strange'] = True
 
@@ -31,3 +34,5 @@ def passcard_info_view(request, passcode):
 def is_visit_long(visit, minutes=60):
     if visit.leaved_at:
         return visit.leaved_at - visit.entered_at > datetime.timedelta(minutes=minutes)
+    else:
+        return timezone.now() - visit.entered_at > datetime.timedelta(minutes=minutes)
